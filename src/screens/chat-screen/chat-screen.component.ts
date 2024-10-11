6b4de83115a05f53e9e7be5828e499c041079e9d
Nils <nils.verneret@gmail.com>
2024-10-11 10:20:54 +0200

    Change scss structure //In src/_global_style

--- src/screens/chat-screen/chat-screen.component.ts
+++ src/screens/chat-screen/chat-screen.component.ts
@@ -3,29 +3,35 @@ import { ChatService } from '../../services/chat.service';
 import { ChatBubbleComponent } from './components/chat-bubble/chat-bubble.component';
 import { UserService } from '../../services/user.service';
 import { TConversation } from '../../_models/conversation.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chat-screen',
   standalone: true,
-  imports: [ChatBubbleComponent],
+  imports: [ChatBubbleComponent, FormsModule],
   templateUrl: './chat-screen.component.html',
-  styleUrl: './chat-screen.component.scss'
+  styleUrl: './chat-screen.component.scss',
 })
-
-
 export class ChatScreenComponent {
+  @Input({ required: true }) selectedConversation!: TConversation;
 
-  @Input({required: true}) selectedConversation! : TConversation
+  messageToSend = '';
 
-constructor( public chatService: ChatService, public userService: UserService) {}
-messages = computed(() => {
-return this.chatService.getMessages()
-})
+  constructor(
+    public chatService: ChatService,
+    public userService: UserService
+  ) {}
 
-actualUser = computed(() => {
-  return this.userService.getThisUserId()
-})
+  messages = computed(() => {
+    return this.chatService.getMessages();
+  });
+
+  actualUser = computed(() => {
+    return this.userService.getThisUserId();
+  });
 
 
-
-};
+  dev() {
+    console.log(this.messageToSend)
+  }
+}
